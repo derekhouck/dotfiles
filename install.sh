6e97d054c9528a67f4e6abf3baa7f0bdf856f91a
Derek Houck <derek@derekhouck.com>
2017-07-21 14:43:19 -0700

    Switch clone command to GitHub repo

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 dotfiles="$HOME/dotfiles"
 if [ ! -e $dotfiles/.git ]; then
 	echo "Cloning dotfiles\n"
-	git clone --depth=1 https://tainitam@bitbucket.org/tainitam/dotfiles.git $dotfiles
+	git clone --depth=1 https://github.com/derekhouck/dotfiles.git $dotfiles
 	ln -s "$HOME/dotfiles/.bash_profile" .bash_profile
 else
 	echo "Updating dotfiles\n"
